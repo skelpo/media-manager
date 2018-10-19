85c51f017ead4e644504566d77649fa260f3b68e
Bastian Inuk Christensen <jonalist@gmail.com>
2018-10-19 22:44:27 +0200

    I can upload files with the route /s3/post however the file gets corrupted on the way to S3

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -17,12 +17,14 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
-    try services.register(s3: S3Signer.Config(accessKey: "",
-                                          secretKey: "",
-                                          region: Region(name: Region.RegionName(rawValue: "")!,
-                                                         hostName: "",
-                                                         useTLS: false)), defaultBucket: "my-bucket")
+    // Git message:
+    let accessKey = Environment.get("AWSACCESSKEY")
+    let secretKey = Environment.get("AWSSECRETKEY")
+    let region = Region.euCentral1
+    try services.register(s3: S3Signer.Config(accessKey: accessKey!,
+                                              secretKey: secretKey!,
+                                              region: region),
+                          defaultBucket: "bucket-inuk")
     
     // Configure a SQLite database
     let sqlite = try SQLiteDatabase(storage: .memory)
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,12 +11,16 @@ public func routes(_ router: Router) throws {
     let s3Routes = router.grouped("s3")
     
     func postS3(_ req: Request) throws -> Future<Response> {
-        return try req.content.decode(File.self).map{ file in
+        return try req.content.decode(Filer.self).flatMap { filer in
+            let file = filer.file
+            //print(file.filename)
             let s3 = try req.makeS3Client()
             let string = file.data.base32EncodedString()
+            print(string)
             let fileName = file.filename
-            return try s3.put(string: string, destination: fileName, access: .publicRead, on: req).flatMap(to: Response.self) {_ in 
-                req.redirect(to: "")
+            return try s3.put(string: string, destination: fileName, access: .publicRead, on: req)
+                .map(to: Response.self) {_ in
+                    return req.redirect(to: "")
             }
         }
         
@@ -25,4 +29,10 @@ public func routes(_ router: Router) throws {
     s3Routes.post("post", use: postS3)
     
     
+}
+
+struct Filer: Codable
+{
+    let conformance: String?
+    let file: Core.File
 }

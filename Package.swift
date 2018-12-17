6e3a8178629425be9fc192b6423bfc04545f8e53
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-17 14:56:41 -0600

    Installed skelpo/Storage package

--- Package.swift
+++ Package.swift
@@ -8,12 +8,13 @@ let package = Package(
         .library(name: "App", targets: ["App"])
     ],
     dependencies: [
+        .package(url: "https://github.com/skelpo/Storage.git", from: "0.1.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor-community/vapor-ext.git", from: "0.1.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "ServiceExt"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "ServiceExt", "Storage"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]

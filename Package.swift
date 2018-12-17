e37d7529276e3e6d65f9e46489df15fdacd1019c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-17 14:19:50 -0600

    Removed B2 target and S3 dependency

--- Package.swift
+++ Package.swift
@@ -1,26 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
     name: "media-manager",
     products: [
-        .library(name: "MediaManager", targets: ["MediaManager"]),
-        .library(name: "B2", targets: ["B2"])
+        .library(name: "MediaManager", targets: ["MediaManager"])
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-        
-        .package(url: "https://github.com/LiveUI/S3.git", .branch("master")),
         .package(url: "https://github.com/vapor-community/vapor-ext.git", from: "0.1.0"),
     ],
     targets: [
-        .target(name: "MediaManager", dependencies: ["Vapor", "S3"]),
-        .target(name: "B2", dependencies: ["Vapor"]),
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "MediaManager", "S3", "B2", "ServiceExt"]),
+        .target(name: "MediaManager", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "MediaManager", "ServiceExt"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]

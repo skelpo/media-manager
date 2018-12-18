1264605142083b061451c231bd3164574ed3b02d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-18 10:54:27 -0600

    Created StorageControllerCache service

---
+++ Sources/App/Services/StorageControllerCache.swift
@@ -0,0 +1,24 @@
+import Storage
+import Vapor
+
+class StorageControllerCache: Service {
+    private var factories: [String: (Container)throws -> RouteCollection] = [:]
+    
+    func register<S>(storage: S.Type, slug: String) where S: Storage & ServiceType {
+        let factory = { (container: Container)throws -> StorageController<S> in
+            let storage = try storage.makeService(for: container)
+            let controller = StorageController<S>.init(id: slug, storage: storage)
+            
+            return controller
+        }
+        
+        self.factories[String(describing: S.self)] = factory
+    }
+    
+    func register(to router: Router, on container: Container)throws {
+        let factories = self.factories.values
+        let controllers = try factories.map { factory in try factory(container) }
+        
+        try controllers.forEach(router.register(collection:))
+    }
+}
